6c5331ba216e433c7004721ae068d7a92efe6082
Danylo Kasianenko <d.k@smoothr.de>
2023-05-03 10:18:21 +0300

    Improve DDP seeding

--- train.py
+++ train.py
@@ -1,80 +1,86 @@
-import gc
-import os
+if __name__ == '__main__':
+    import gc
+    import os
 
-import pytorch_lightning as pl
-import torch
-import wandb
-from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.loggers.logger import DummyLogger
+    import pytorch_lightning as pl
+    import torch
+    from pytorch_lightning.loggers import WandbLogger
+    from pytorch_lightning.loggers.logger import DummyLogger
 
-import constants
-from dataset.data_module import LightData
-from module import Module
-from state import state
-from utils import numerated_folder, seed_everything
+    import constants
+    import wandb
+    from dataset.data_module import LightData
+    from module import Module
+    from state import state
+    from utils import numerated_folder, seed_everything
 
-torch.set_float32_matmul_precision('medium')
+    torch.set_float32_matmul_precision('medium')
 
-for seed in range(1):
-    gc.collect()
-    torch.cuda.empty_cache()
-    torch.cuda.ipc_collect()
-    seed_everything(seed)
+    local_rank = int(os.environ.get('LOCAL_RANK', 0))
 
-    data = LightData(
-        os.path.join(constants.DATASET_PATH, 'asl-signs', 'train.csv'), constants.N_COORDS, constants.MAX_LEN,
-        constants.BATCH_SIZE, constants.STEPS_PER_EPOCH, constants.EPOCHS,
-        constants.WORKERS, constants.EXTERNAL_DATASETS
-    )
+    for seed in range(1):
+        gc.collect()
+        torch.cuda.empty_cache()
+        torch.cuda.ipc_collect()
+        seed_everything(seed | (local_rank << 16))
 
-    working_dir = numerated_folder(os.path.join(constants.MODELS_PATH, 'asl'))
-    session_name = os.path.basename(working_dir)
-    module = Module(
-        working_dir, constants.EPOCHS, constants.STEPS_PER_EPOCH, constants.LR, data.n_features, constants.N_FEATURES,
-        data.n_outputs, constants.MAX_LEN, constants.DROPOUT,
-        constants.N_LAYERS, constants.N_HEADS, constants.MLP_RATIO, constants.ARCHITECTURE, constants.CAUSAL_FORESIGHT,
-        constants.EMA, constants.LABEL_SMOOTHING, constants.MIXUP, constants.N_COORDS
-    )
-    if constants.CHECKPOINT_PATH:
-        module.load_state_dict(torch.load(constants.CHECKPOINT_PATH)['state_dict'], strict=False)
-
-    if constants.TEACHER_PATH:
-        module.teacher = Module.load_from_checkpoint(constants.TEACHER_PATH, 'cpu')
-
-    trainer = pl.Trainer(
-        enable_checkpointing=True,
-        max_epochs=constants.EPOCHS,
-        check_val_every_n_epoch=1,
-        accelerator='gpu',
-        devices=constants.DEVICES,
-        logger=WandbLogger(session_name, project='ASL') if constants.WORKERS else DummyLogger(),
-        strategy='ddp_find_unused_parameters_false' if isinstance(constants.DEVICES, list) or constants.DEVICES > 1
-        else 'auto',
-        precision='bf16',
-        accumulate_grad_batches=constants.ACCUMULATE,
-        gradient_clip_val=1.,
-        callbacks=[
-                      pl.callbacks.LearningRateMonitor(logging_interval='epoch', log_momentum=True),
-                      pl.callbacks.ModelCheckpoint(
-                          working_dir, filename=session_name, monitor='val/acc', mode='max', save_top_k=1,
-                          save_last=True,
-                      )
-                  ] + ([pl.callbacks.QuantizationAwareTraining('qnnpack')] if constants.QAT else []),
-    )
-    if constants.TASK == 'find_lr':
-        result = trainer.tune(module, data, lr_find_kwargs={'num_training': 100})
-        if state['master']:
-            result['lr_find'].plot(show=True)
-            print(result['lr_find'].suggestion())
-    elif constants.TASK == 'train':
-        trainer.fit(
-            module, data,
-            ckpt_path=constants.CHECKPOINT_PATH
-            if constants.CHECKPOINT_PATH.endswith('.ckpt') and constants.RESUME else None,
+        data = LightData(
+            os.path.join(constants.DATASET_PATH, 'asl-signs', 'train.csv'), constants.N_COORDS, constants.MAX_LEN,
+            constants.BATCH_SIZE, constants.STEPS_PER_EPOCH, constants.EPOCHS,
+            constants.WORKERS, constants.EXTERNAL_DATASETS
         )
-    elif constants.TASK == 'val':
-        trainer.validate(module, data)
-    else:
-        raise ValueError(f'Unknown task: {repr(constants.TASK)}')
 
-    wandb.finish()
+        working_dir = numerated_folder(os.path.join(constants.MODELS_PATH, 'asl'), create=local_rank == 0)
+        session_name = os.path.basename(working_dir)
+        module = Module(
+            working_dir, constants.EPOCHS, constants.STEPS_PER_EPOCH, constants.LR, data.n_features,
+            constants.N_FEATURES,
+            data.n_outputs, constants.MAX_LEN, constants.DROPOUT,
+            constants.N_LAYERS, constants.N_HEADS, constants.MLP_RATIO, constants.ARCHITECTURE,
+            constants.CAUSAL_FORESIGHT,
+            constants.EMA, constants.LABEL_SMOOTHING, constants.MIXUP, constants.MIXUP_ELEMENTS, constants.N_COORDS
+        )
+        if constants.CHECKPOINT_PATH:
+            module.load_state_dict(torch.load(constants.CHECKPOINT_PATH)['state_dict'], strict=False)
+
+        if constants.TEACHER_PATH:
+            module.teacher = Module.load_from_checkpoint(constants.TEACHER_PATH, 'cpu')
+
+        trainer = pl.Trainer(
+            enable_checkpointing=True,
+            max_epochs=constants.EPOCHS,
+            check_val_every_n_epoch=1,
+            accelerator='gpu',
+            devices=constants.DEVICES,
+            logger=WandbLogger(session_name, project='ASL') if constants.WORKERS else DummyLogger(),
+            strategy=('ddp_find_unused_parameters_true' if constants.CAUSAL_FRACTION
+                      else 'ddp_find_unused_parameters_false')
+            if isinstance(constants.DEVICES, list) or constants.DEVICES > 1 else 'auto',
+            precision='16-mixed' if constants.ARCHITECTURE == 'lstm' else 'bf16-mixed',
+            accumulate_grad_batches=constants.ACCUMULATE,
+            gradient_clip_val=1.,
+            callbacks=[
+                          pl.callbacks.LearningRateMonitor(logging_interval='epoch', log_momentum=True),
+                          pl.callbacks.ModelCheckpoint(
+                              working_dir, filename=session_name, monitor='val/acc', mode='max', save_top_k=1,
+                              save_last=True,
+                          )
+                      ] + ([pl.callbacks.QuantizationAwareTraining('qnnpack')] if constants.QAT else []),
+        )
+        if constants.TASK == 'find_lr':
+            result = trainer.tune(module, data, lr_find_kwargs={'num_training': 100})
+            if state['master']:
+                result['lr_find'].plot(show=True)
+                print(result['lr_find'].suggestion())
+        elif constants.TASK == 'train':
+            trainer.fit(
+                module, data,
+                ckpt_path=constants.CHECKPOINT_PATH
+                if constants.CHECKPOINT_PATH.endswith('.ckpt') and constants.RESUME else None,
+            )
+        elif constants.TASK == 'val':
+            trainer.validate(module, data)
+        else:
+            raise ValueError(f'Unknown task: {repr(constants.TASK)}')
+
+        wandb.finish()
